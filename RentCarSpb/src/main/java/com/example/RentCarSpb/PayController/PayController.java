41c4263f5d8d1fc927f856140afa71f6958d3c53
Howard <iamxyzgos5933@gmail.com>
2024-09-03 10:15:12 +0800

    .

--- RentCarSpb/src/main/java/com/example/RentCarSpb/PayController/PayController.java
+++ RentCarSpb/src/main/java/com/example/RentCarSpb/PayController/PayController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.RentCarSpb.Dto.PayDTO; 
 import com.example.RentCarSpb.Entity.Paydb;
+import com.example.RentCarSpb.Entity.RentFormdb;
 //import com.example.RentCarSpb.Entity.RentFormdb;
 import com.example.RentCarSpb.Service.PayService;
 
--- RentCarSpb/src/main/java/com/example/RentCarSpb/RentFormController/RentFormController.java
+++ RentCarSpb/src/main/java/com/example/RentCarSpb/RentFormController/RentFormController.java
@@ -4,6 +4,7 @@ package com.example.RentCarSpb.RentFormController;
 import java.util.List;
 import java.util.Optional;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
