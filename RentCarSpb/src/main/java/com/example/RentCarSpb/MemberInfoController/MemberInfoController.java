5228111d0f7e02520810a6ad3ab0eda2d1ac7bcf
Howard <iamxyzgos5933@gmail.com>
2024-09-10 15:54:59 +0800

    .

--- RentCarSpb/src/main/java/com/example/RentCarSpb/MemberInfoController/MemberInfoController.java
+++ RentCarSpb/src/main/java/com/example/RentCarSpb/MemberInfoController/MemberInfoController.java
@@ -1,14 +1,12 @@
 package com.example.RentCarSpb.MemberInfoController;
 
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- RentCarSpb/src/main/java/com/example/RentCarSpb/RentFormController/RentFormController.java
+++ RentCarSpb/src/main/java/com/example/RentCarSpb/RentFormController/RentFormController.java
@@ -4,7 +4,6 @@ package com.example.RentCarSpb.RentFormController;
 import java.util.List;
 import java.util.Optional;
 
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
